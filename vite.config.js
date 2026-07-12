37491293f18cf86da4e71127605d132a399162f5
nichoth <nichoth@nichoth.com>
2026-07-12 16:15:46 -0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +12,7 @@ export default defineConfig({
     },
     publicDir: '_public',
     css: {
-        postcss: {
-            plugins: [
-                postcssNesting
-            ],
-        },
+        transformer: 'lightningcss'
     },
     server: {
         port: 1111,
